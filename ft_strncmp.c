9a0e9f03e67ea752dea5975e2eded01e5ac9ad47
Beatriz <beatrizoffline@gmail.com>
2024-09-04 11:28:17 +0100

    changed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -21,6 +21,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned int	i;
 
 	i = 0;
+	//feature added for fractol
+	if (s1[0] == '\0' || s2[0] == '\0')
+		return (1);
 	if (n == 0)
 		return (0);
 	while (i < n - 1 && s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
